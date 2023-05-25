14e4f4a8b78acdc52f813392c963c3f9519daec5
franznowak <franz.g.nowak@gmail.com>
2023-05-25 15:26:27 +0200

    change indexing of spans to match paper, add more tests

--- fastlri/parsing/parser.py
+++ fastlri/parsing/parser.py
@@ -20,19 +20,19 @@ class Parser:
 
             # terminal productions
             for (head, body), w in self.cfg.terminal:
-                for i in range(N):
-                    if body[0] == input[i]:
-                        β[head, i, i+1] += w
+                for k in range(N):
+                    if body[0] == input[k]:
+                        β[head, k, k] += w
 
             # binary productions
-            for l in range(1, N+2):
+            for l in range(2, N+1):
                 for i in range(N-l+1):
-                    k = i + l
-                    for j in range(i+1, k):
+                    k = i + l - 1
+                    for j in range(i, k):
                         for p, w in self.cfg.binary:
                             X, Y, Z = p.head, p.body[0], p.body[1]
-                            β[X, i, k] += β[Y, i, j] * β[Z, j, k] * w
-            return β if chart else β[S, 0, N]
+                            β[X, i, k] += β[Y, i, j] * β[Z, j+1, k] * w
+            return β if chart else β[S, 1, N]
     
     def cky_fast(self, input, chart=False):
         """A faster version of CKY  for dense grammars."""
@@ -53,22 +53,22 @@ class Parser:
 
         # terminal productions
         for (head, body), w in self.cfg.terminal:
-            for i in range(N):
-                if body[0] == input[i]:
-                    β[head, i, i+1] += w
+            for k in range(N):
+                if body[0] == input[k]:
+                    β[head, k, k] += w
 
         # binary productions
-        for span in range(2, N+1):
-            for i in range(N-span+1):
-                k = i + span
+        for l in range(2, N+1):
+            for i in range(N-l+1):
+                k = i + l - 1
                 for Y in self.cfg.V:
                     for Z in self.cfg.V:
                         γ = 0.0
-                        for j in range(i+1, k):
-                            γ += β[Y, i, j] * β[Z, j, k]
+                        for j in range(i, k):
+                            γ += β[Y, i, j] * β[Z, j+1, k]
                         for X in self.cfg.V:
                             β[X, i, k] += γ * W[X, Y, Z]
-        return β if chart else β[S, 0, N]
+        return β if chart else β[S, 1, N]
 
     def plc(self):
         """Computes the left-corner expectations. Requires CNF."""
@@ -113,30 +113,30 @@ class Parser:
         # precompute E2
         E2 = dd(lambda: 0.0)
         for X in self.cfg.V:
-            for p, w in self.cfg.binary:
-                Y2, Y, Z = p.head, p.body[0], p.body[1]
+            for (head, body), w in self.cfg.binary:
+                Y2, Y, Z = head, body[0], body[1]
                 E2[X, Y, Z] += E[X, Y2] * self.cfg._P[Production(Y2, (Y, Z))]
 
         # compute base case
         for X in self.cfg.V:
             for i in range(N):
-                for p, w in self.cfg.terminal:
-                    Y, v = p.head, p.body[0]
+                for (head, body), w in self.cfg.terminal:
+                    Y, v = head, body[0]
                     if v == input[i]:
-                        ppre[X, i, i+1] += E[X,Y] * w
+                        ppre[X, i, i] += E[X, Y] * w
 
         # compute prefix probability
-        for l in range(1, N+2):
-            for i in range(N - l + 1):
-                k = i + l
-                for j in range(i+1, k):
+        for l in range(2, N+1):
+            for i in range(N-l+1):
+                k = i + l - 1
+                for j in range(i, k):
                     for X in self.cfg.V:
                         for Y in self.cfg.V:
                             for Z in self.cfg.V:
                                 ppre[X, i, k] += E2[X, Y, Z] * β[Y, i, j] \
-                                    * ppre[Z, j, k]
+                                    * ppre[Z, j+1, k]
         
-        return ppre if chart else ppre[S, 0, N]
+        return ppre if chart else ppre[S, 1, N]
     
     def lri_fast(self, input, chart=False):
         """Faster prefix parsing algorithm by Nowak and Cotterell (2023)."""
@@ -179,15 +179,15 @@ class Parser:
                 for p, w in self.cfg.terminal:
                     Y, v = p.head, p.body[0]
                     if v == input[i]:
-                        ppre[X, i, i+1] += E[X, Y] * w
+                        ppre[X, i, i] += E[X, Y] * w
 
         # compute prefix probability
-        for l in range(1, N+2):
-            for i in range(N - l + 1):
-                k = i + l
-                for j in range(i+1, k):
+        for l in range(2, N+1):
+            for i in range(N-l+1):
+                k = i + l - 1
+                for j in range(i, k):
                     for X in self.cfg.V:
                         for Z in self.cfg.V:
-                            ppre[X, i, k] += δ[i, j, X, Z] * ppre[Z, j, k]
+                            ppre[X, i, k] += δ[i, j, X, Z] * ppre[Z, j+1, k]
         
-        return ppre if chart else ppre[S, 0, N]
+        return ppre if chart else ppre[S, 1, N]
--- test/test_lri.py
+++ test/test_lri.py
@@ -4,34 +4,102 @@ from fastlri.base.nonterminal import NT
 from fastlri.base.symbol import Sym
 from fastlri.parsing.parser import Parser
 
-Y = NT("Y")
-Z = NT("Z")
-a = Sym("a")
 
-cfg = CFG()
-cfg.add(1, cfg.S, Y, Z)
-cfg.add(0.5, Y, Z, Y)
-cfg.add(0.5, Y, a)
-cfg.add(1, Z, a)
-parser = Parser(cfg)
+def get_simple_cfg():
+    Y = NT("Y")
+    Z = NT("Z")
+    a = Sym("a")
+
+    cfg = CFG()
+    cfg.add(1, cfg.S, Y, Z)
+    cfg.add(0.5, Y, Z, Y)
+    cfg.add(0.5, Y, a)
+    cfg.add(1, Z, a)
+
+    return cfg
+
+def get_complex_cfg():
+    NP = NT("NP")
+    VP = NT("VP")
+    Det = NT("Det")
+    N = NT("N")
+    PP = NT("PP")
+    V = NT("V")
+    Adj = NT("Adj")
+    Adv = NT("Adv")
+    AdvP = NT("AdvP")
+    fruit = Sym("fruit")
+    flies = Sym("flies")
+    like = Sym("like")
+    a = Sym("a")
+    green = Sym("green")
+    banana = Sym("banana")
+
+    cfg = CFG()
+    cfg.add(1, cfg.S, NP, VP)
+    cfg.add(0.25, NP, Det, N)
+    cfg.add(0.25, NP, Det, NP)
+    cfg.add(0.25, NP, N, N)
+    cfg.add(0.25, NP, Adj, N)
+    cfg.add(1, VP, V, NP)
+    cfg.add(1, AdvP, Adv, NP)
+    cfg.add(0.5, N, fruit)
+    cfg.add(0.25, N, flies)
+    cfg.add(0.25, N, banana)
+    cfg.add(0.5, V, flies)
+    cfg.add(0.5, V, like)
+    cfg.add(1, Det, a)
+    cfg.add(1, Adj, green)
+    cfg.add(1, Adv, like)
+    
+    return cfg
 
 class TestLri:
     def test_plc(self):
+        parser = Parser(get_simple_cfg())
         P_L = parser.plc()
         correct = np.array([[1. , 1. , 0.5],\
                             [0. , 1. , 0.5],\
                             [0. , 0. , 1. ]])
         assert np.all(P_L == correct)
 
+    def test_cky(self):
+        parser = Parser(get_complex_cfg())
+        pins = parser.cky("fruit flies", chart=True)
+        assert pins[(NT("N"), 0, 0)] == 0.5
+        assert pins[(NT("N"), 1, 1)] == 0.25
+        assert pins[(NT("NP"), 0, 1)] == 0.03125
+
+    def test_cky_fast(self):
+        parser = Parser(get_complex_cfg())
+        pins = parser.cky_fast("fruit flies", chart=True)
+        assert pins[(NT("N"), 0, 0)] == 0.5
+        assert pins[(NT("N"), 1, 1)] == 0.25
+        assert pins[(NT("NP"), 0, 1)] == 0.03125
+
+    
     def test_lri(self):
-        ppre = parser.lri([a,a,a], chart=True)
+        parser = Parser(get_simple_cfg())
+        ppre = parser.lri("a a a", chart=True)
+        assert ppre[(parser.cfg.S, 0, 0)] == 1.0
         assert ppre[(parser.cfg.S, 0, 1)] == 1.0
-        assert ppre[(parser.cfg.S, 0, 2)] == 1.0
-        assert ppre[(parser.cfg.S, 0, 3)] == 0.5
-        
+        assert ppre[(parser.cfg.S, 0, 2)] == 0.5
+
+        parser = Parser(get_complex_cfg())
+        ppre = parser.lri("fruit flies", chart=True)
+        assert ppre[(parser.cfg.S, 0, 0)] == 0.125
+        assert ppre[(parser.cfg.S, 1, 1)] == 0.0625
+        assert ppre[(parser.cfg.S, 0, 1)] == 0.03125
 
     def test_lri_fast(self):
-        ppre = parser.lri_fast([a,a,a], chart=True)
+        parser = Parser(get_simple_cfg())
+        ppre = parser.lri_fast("a a a", chart=True)
+        assert ppre[(parser.cfg.S, 0, 0)] == 1.0
         assert ppre[(parser.cfg.S, 0, 1)] == 1.0
-        assert ppre[(parser.cfg.S, 0, 2)] == 1.0
-        assert ppre[(parser.cfg.S, 0, 3)] == 0.5
+        assert ppre[(parser.cfg.S, 0, 2)] == 0.5
+
+        parser = Parser(get_complex_cfg())
+        ppre = parser.lri_fast("fruit flies", chart=True)
+        assert ppre[(parser.cfg.S, 0, 0)] == 0.125
+        assert ppre[(parser.cfg.S, 1, 1)] == 0.0625
+        assert ppre[(parser.cfg.S, 0, 1)] == 0.03125
